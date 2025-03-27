9a1c05ea6a6656240de28198cb285add9faf80f4
Justin Gwinn <gwinnj3@gmail.com>
2025-03-26 20:53:53 -0500

    made the thing

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,5 @@
+
+export default function Blogs() {
+    return <h1 className="text-4xl font-extralight tracking-wider text-gray-800 mb-6">Hello there</h1>;
+  };
+  
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,4 @@
+
+export default function Contact() {
+    return <h1 className="text-4xl font-serif italic font-medium text-amber-800 border-b-2 border-amber-200 pb-3">This is a contact page</h1>;
+  };
